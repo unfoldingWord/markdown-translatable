fb857fd823d857e6e75f9dc07b0dea5b1ad0e9bc
RoyalSix <jay.a.scottjr@biola.edu>
2020-01-27 14:23:52 -0800

    added implementation for rendering html raw

--- src/core/markdown-converter.ts
+++ src/core/markdown-converter.ts
@@ -1,7 +1,7 @@
 import TurndownService from 'turndown';
 import { gfm } from 'turndown-plugin-gfm';
-import { filter } from './string';
 import showdown from 'showdown';
+import { filter } from './string';
 
 const turndownService = new TurndownService({ headingStyle: 'atx' });
 turndownService.use(gfm);
@@ -11,19 +11,27 @@ turndownService.addRule('strikethrough', {
 });
 turndownService.addRule('underline', {
   filter: ['u'],
-  replacement: (content) => `__${content}__`,
+  replacement: (content) => `<u>${content}</u>`,
 });
 turndownService.addRule('emphasis', {
   filter: ['em'],
   replacement: (content) => `*${content}*`,
 });
+
 const markdownToHtmlConverter = new showdown.Converter();
 markdownToHtmlConverter.setOption('underline', true);
+
+export const toDisplay = (content) => content.replace(/&/g, '&amp;').replace(/</g, '&lt;');
+
+export const fromDisplay = (content) => content.replace(/&lt;/g, '<').replace(/&amp;/g, '&');
 
 export const htmlToMarkdown = ({ html, outputFilters = [] }) => {
   let markdown = turndownService.turndown(html);
   markdown = filter({ string: markdown, filters: outputFilters });
-  if (markdown === '&#8203;') markdown = '';
+
+  if (markdown === '&#8203;') {
+    markdown = '';
+  }
   return markdown;
 };
 
@@ -31,6 +39,9 @@ export const markdownToHtml = ({ markdown, inputFilters = [] }) => {
   let _markdown = markdown.slice(0);
   _markdown = filter({ string: _markdown, filters: inputFilters });
   let html = markdownToHtmlConverter.makeHtml(_markdown);
-  if (!html || html === '') html = '<p>&#8203;</p>';
+
+  if (!html || html === '') {
+    html = '<p>&#8203;</p>';
+  }
   return html;
 };
