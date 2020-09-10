3f1298d3cfec587e9cf83e09b99b33e611cc7dd0
RoyalSix <jay.a.scottjr@biola.edu>
2020-09-10 11:33:08 -0700

    wip

--- src/components/block-editable/BlockEditable.js
+++ src/components/block-editable/BlockEditable.js
@@ -1,13 +1,15 @@
-import React, { useMemo, useCallback } from 'react';
+import React, {
+  useMemo, useCallback, useEffect, useRef, useState,
+} from 'react';
 import PropTypes from 'prop-types';
 
-import * as helpers from '../../core/';
 import {
   markdownToHtml,
   htmlToMarkdown,
   filter,
   toDisplay,
   fromDisplay,
+  isHebrew,
 } from '../../core/';
 import { useStyles } from './useStyles';
 
@@ -18,22 +20,36 @@ function BlockEditable({
   outputFilters,
   style,
   preview,
-  editable,
+  editable='true',
 }) {
-  const _oldMarkdown = { markdown };
+  useEffect(() => {
+    const markdownEditable = document.getElementById('markdown-editable');
 
+    if (markdownEditable) {
+      let code = filter({ string: markdown, filters: inputFilters });
+      code = toDisplay(code);
+      markdownEditable.innerText = code;
+    }
+
+    const htmlEditable = document.getElementById('html-editable');
+
+    if (htmlEditable) {
+      htmlEditable.innerHTML = markdownToHtml({ markdown, inputFilters });
+    }
+  }, [inputFilters, markdown]);
+
+  const [oldMarkdown, setOldMarkdown] = useState(markdown);
   const classes = useStyles();
-
   const _style = useMemo(
     () =>
-      helpers.isHebrew(markdown) ? { ...style, fontSize: '1.5em' } : style,
+      isHebrew(markdown) ? { ...style, fontSize: '1.5em' } : style,
     [style, markdown]
   );
 
-  const handleBlur = useCallback(
+  const handleChange = useCallback(
     (_markdown) => {
       const oldHTML = markdownToHtml({
-        markdown: _oldMarkdown.markdown,
+        markdown: oldMarkdown,
         inputFilters: inputFilters,
       });
       const newHTML = markdownToHtml({
@@ -41,72 +57,59 @@ function BlockEditable({
         inputFilters: inputFilters,
       });
 
-      _oldMarkdown.markdown = _markdown;
+      setOldMarkdown(_markdown);
 
-      if (oldHTML !== newHTML) onEdit(_markdown);
+      if (oldHTML !== newHTML) {
+        onEdit(_markdown);
+      }
     },
-    [markdown, inputFilters, onEdit]
+    [inputFilters, oldMarkdown, onEdit]
   );
 
-  const handleHTMLBlur = useCallback(
+  const handleHTMLChange = useCallback(
     (e) => {
       const html = e.target.innerHTML;
       const _markdown = htmlToMarkdown({ html, outputFilters });
-      handleBlur(_markdown);
+      handleChange(_markdown, e);
     },
-    [handleBlur, outputFilters]
+    [handleChange, outputFilters]
   );
 
-  const handleRawBlur = useCallback(
+
+  const handleRawChange = useCallback(
     (e) => {
       let string = e.target.innerText;
       string = fromDisplay(string);
       const _markdown = filter({ string, filters: outputFilters });
-      handleBlur(_markdown);
+      handleChange(_markdown);
     },
-    [handleBlur, outputFilters]
+    [handleChange, outputFilters]
   );
 
-  const component = useMemo(() => {
-    let _component;
-
-    if (!preview) {
-      let code = filter({ string: markdown, filters: inputFilters });
-      code = toDisplay(code);
-      const dangerouslySetInnerHTML = { __html: code };
-
-      _component = (
-        <pre className={classes.pre}>
-          <code
-            className={classes.markdown}
-            style={_style}
-            dir='auto'
-            contentEditable={editable}
-            onBlur={handleRawBlur}
-            dangerouslySetInnerHTML={dangerouslySetInnerHTML}
-          />
-        </pre>
-      );
-    } else {
-      const dangerouslySetInnerHTML = {
-        __html: markdownToHtml({ markdown, inputFilters }),
-      };
-
-      _component = (
-        <div
-          style={_style}
-          className={classes.html}
+  return (
+    <div className={classes.root}>
+      <pre className={classes.pre} style={{ display: !preview ? 'block' : 'none' }}>
+        <code
+          id="markdown-editable"
           dir='auto'
+          style={_style}
+          className={classes.markdown}
           contentEditable={editable}
-          dangerouslySetInnerHTML={dangerouslySetInnerHTML}
-          onBlur={handleHTMLBlur}
+          onInput={handleRawChange}
+          suppressContentEditableWarning={true}
         />
-      );
-    }
-    return _component;
-  }, [preview, markdown, editable]);
-
-  return <div className={classes.root}>{component}</div>;
+      </pre>
+      <div
+        id="html-editable"
+        dir='auto'
+        style={{ ..._style, display: preview ? 'block' : 'none' }}
+        className={classes.html}
+        contentEditable={editable}
+        onInput={handleHTMLChange}
+        suppressContentEditableWarning={true}
+      />
+    </div>
+  );
 }
 
 BlockEditable.propTypes = {
--- src/core/string.ts
+++ src/core/string.ts
@@ -13,3 +13,48 @@ export const isHebrew = (string) => {
   const percent = hebrewChars.length / string.length;
   return percent > 0.75;
 };
+
+function getCaretPosition(editableDiv) {
+  let caretPos = 0,
+    sel,
+    range;
+
+  if (window.getSelection) {
+    sel = window.getSelection();
+
+    if (sel.rangeCount) {
+      range = sel.getRangeAt(0);
+
+      if (range.commonAncestorContainer.parentNode == editableDiv) {
+        caretPos = range.endOffset;
+      }
+    }
+  } else if (document.selection && document.selection.createRange) {
+    range = document.selection.createRange();
+
+    if (range.parentElement() == editableDiv) {
+      const tempEl = document.createElement("span");
+      editableDiv.insertBefore(tempEl, editableDiv.firstChild);
+      const tempRange = range.duplicate();
+      tempRange.moveToElementText(tempEl);
+      tempRange.setEndPoint("EndToEnd", range);
+      caretPos = tempRange.text.length;
+    }
+  }
+  return caretPos;
+}
+
+export const getCursorReset = (e) => {
+  const el = e.target;
+  const position = getCaretPosition(el);
+
+  return () => {
+    const range = document.createRange();
+    const sel = window.getSelection();
+    range.setStart(el.childNodes[0], 10);
+    range.collapse(true);
+    sel.removeAllRanges();
+    sel.addRange(range);
+    el.focus();
+  };
+};
