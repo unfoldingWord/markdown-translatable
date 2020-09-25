3e56da338b804c25a0bd978f7f67b2ed2767b773
RoyalSix <jay.a.scottjr@biola.edu>
2020-09-25 11:21:59 -0700

    removed console.log

--- src/components/block-editable/BlockEditable.js
+++ src/components/block-editable/BlockEditable.js
@@ -79,7 +79,6 @@ function BlockEditable(props) {
 
 
   const _style = isHebrew(markdown) ? { ...style, fontSize: '1.5em' } : style;
-  console.log("htmlDisplay", htmlDisplay)
   return (
     <div className={classes.root}>
       {!preview &&
