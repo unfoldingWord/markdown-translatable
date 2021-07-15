e74194d99fba716dcdd9a71ace97a338db2a523d
ancientTexts-net <zach.pennington@unfoldingWord.org>
2021-07-14 19:50:02 -0500

    remove unused import

--- src/components/Markdown.context.js
+++ src/components/Markdown.context.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const MarkdownContext = React.createContext({ state: {}, actions: [] });
